e2b3c0a355b4a53ecc470048002678b5874bbbe8
Urdo <karol.ugarenko@gmail.com>
2022-02-14 22:04:34 +0100

    job done

--- rollApp.js
+++ rollApp.js
@@ -2,8 +2,8 @@ const button = document.querySelector('button');
 const names = ["David", "John", "Mark", "Steven", "Carl", "Noob", "Karen", "Troy"]; // creat name list
 const nameGenerator = () => {
     
-    console.log(Math.floor(Math.random()  *8)); // Roll 
-    
+    const indexRoll = (Math.floor(Math.random() *8)); // Roll 
+    document.querySelector("div").textContent =names[indexRoll]; //add textContect do div
 }
 
 button.addEventListener("click", nameGenerator)
